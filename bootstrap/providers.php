13f9bde5fd11409b602dc36e9c34eb3c8fecdb4a
Gwentey <anthonyoutub@gmail.com>
2025-03-02 23:27:22 +0100

    init

--- bootstrap/providers.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    // Application Service Providers...
-    App\Providers\AppServiceProvider::class,
-    App\Providers\AuthServiceProvider::class,
-    App\Providers\EventServiceProvider::class,
-    App\Providers\RouteServiceProvider::class,
-];
